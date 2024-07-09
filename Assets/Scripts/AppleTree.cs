10cc2b2f09f021514325e0537a8ccd55344e5a1b
unknown <sashakotik450697@gmail.com>
2024-07-09 12:59:14 +0300

    Update scripts

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -13,7 +13,7 @@ public class AppleTree : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke( "DropApple", 2f ); 
     }
 
     // Update is called once per frame
@@ -22,5 +22,28 @@ public class AppleTree : MonoBehaviour
         Vector3 pos = transform.position;
         pos.x += speed * Time.deltaTime;
         transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        } else if (pos.x > leftAndRightEdge)
+        {
+            speed -= Mathf.Abs(speed);
+        }
+    }
+
+    private void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
     }
 }
